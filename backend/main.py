265e9fffe641ff9efcdfd1164c37110daed464a7
마현우 <mhw35689402@gmail.com>
2023-11-26 00:34:24 +0900

    fix: CORS 이슈

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from service import getLoLHistory
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 load_dotenv()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 @app.get("/{name}")
