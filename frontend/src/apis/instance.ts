25f0de293e7e5f03446db1fa9a0a900ebe87386b
마현우 <mhw35689402@gmail.com>
2023-12-04 13:28:54 +0900

    deploy: api url 변경

--- frontend/src/apis/instance.ts
+++ frontend/src/apis/instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://port-0-lol-analyze-32updzt2alpqds9zr.sel4.cloudtype.app",
+  baseURL: "https://port-0-lol-analyze-backend-32updzt2alpqds9zr.sel4.cloudtype.app",
   timeout: 10000,
 });
 
